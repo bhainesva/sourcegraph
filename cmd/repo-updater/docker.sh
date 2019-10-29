8edb4d22aba110733c5b7b43dd6bef54588e8ea9
ggilmore <geoffrey@sourcegraph.com>
2019-10-29 16:28:54 -0700

    repo-updater

--- cmd/repo-updater/docker.sh
+++ cmd/repo-updater/docker.sh
@@ -3,8 +3,37 @@
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 set -euxo pipefail
 
-docker build -f cmd/repo-updater/Dockerfile -t $IMAGE . \
-    --build-arg COMMIT_SHA \
-    --build-arg DATE \
-    --build-arg VERSION \
-    --build-arg REPO_UPDATER_PKG
+BUILD_ARGS=(
+    "DATE"
+    "COMMIT_SHA"
+    "VERSION"
+    "REPO_UPDATER_PKG"
+)
+
+if [[ "$CI" == "true" ]]; then
+
+    substitutions="_IMAGE=$IMAGE"
+    for arg in "${BUILD_ARGS[@]}"; do
+        if [[ "${!arg}" ]]; then
+            substitutions+=",_${arg}=${!arg}"
+        fi
+    done
+
+    gcloud builds submit --config=cmd/repo-updater/cloudbuild.yaml \
+        --substitutions=$substitutions \
+        --no-source
+else
+
+    build_arg_str=""
+    for arg in "${BUILD_ARGS[@]}"; do
+        if [[ "${!arg}" ]]; then
+            build_arg_str+="--build-arg ${arg}=${!arg} "
+        fi
+    done
+
+    docker build -f cmd/repo-updater/Dockerfile -t $IMAGE . \
+        $build_arg_str \
+        --progress=plain
+
+fi
+
